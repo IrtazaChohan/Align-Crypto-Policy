f1c1257595bb49d6ba3e2168adfe3cb7ad026aa2
Irtaza Chohan <irtazachohan@gmail.com>
2019-11-25 12:25:15 +0000

    Added in Synosis to script

--- Align_Crypto_Policy.ps1
+++ Align_Crypto_Policy.ps1
@@ -9,7 +9,146 @@
 # VERSION HISTORY
 # 1.0 <DATE> Initial Version.
 # 2.0 24/11/2019 Updated script to accept parameters to meet various configurations
+# 2.1 25/11/2019 Added in help section
 # #############################################################################
+
+<#
+
+.SYNOPSIS
+
+This script configures a server to align with best practice crptography levels. 
+
+Written by Irtaza Chohan.
+
+.DESCRIPTION
+
+TLS/SSL and Cryptography levels need to be adhered to and as best practice this script adjusts a server to meet this best practice.
+
+This has been tested on:
+
+- Windows Server 2008
+- Windows Server 2008 R2
+- Windows Server 2012 R2
+- Windows Server 2016
+- Windows Server 2019
+
+Note that registry backup is done as part of this script - the more you run this script the more backup it takes into new files without deleting older backups.
+
+There are a number of parameters that this script accepts based on your requirment - please refer to Parameter section for information on this.
+
+NOTES:
+
+ 1. You need to have Administrative rights on the server to run this script. 
+ 2. This script cannot be run remotely. You will need to copy the folder structure down locally to the server.
+ 3. Please remove the local copy of this script once completed.
+ 
+
+.PARAMETER LogFilePath
+
+Mandatory Parameter
+
+Please specify where on the server you want to keep the logfile that is generated.
+
+.PARAMETER RegistryExportPath
+
+Mandatory Parameter
+
+Please specify where on the server you want to keep the registry backp files that is generated. This script will not remove any older backup files.
+
+.PARAMETER SetDefaultCiphers
+
+Optional Parameter
+
+This option sets the current ciphers levels on the server alongside the setting the correct secure order of them. The ciphers that get set are below (and this is the order):
+
+TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384_P521',
+'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384_P384',
+'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P521',
+'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P384',
+'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256_P256',
+'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P521',
+'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P384',
+'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384_P256',
+'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P521',
+'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P384',
+'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256_P256',
+'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P521',
+'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P384',
+'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA_P256',
+'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P521',
+'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P384',
+'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA_P256',
+'TLS_DHE_RSA_WITH_AES_128_GCM_SHA256',
+'TLS_RSA_WITH_AES_256_GCM_SHA384',
+'TLS_RSA_WITH_AES_128_GCM_SHA256',
+'TLS_DHE_RSA_WITH_AES_256_CBC_SHA',
+'TLS_DHE_RSA_WITH_AES_128_CBC_SHA',
+'TLS_RSA_WITH_AES_128_CBC_SHA256',
+'TLS_RSA_WITH_AES_128_CBC_SHA'
+
+.PARAMETER DisableSSLv3Client
+
+Optional Parameter
+
+This disables SSLv3 client - select this if you want this to happen on the server (highly recommended!)
+
+.PARAMETER DisableSSLv3Server
+
+Optional Parameter
+
+This disables SSLv3 server - select this if you want this to happen on the server (highly recommended!)
+
+.PARAMETER EnableSSLv1
+
+Optional Parameter
+
+This option enables TLSv1.0
+
+.PARAMETER EnableSSLv11
+
+Optional Parameter
+
+This option enables TLSv1.1
+
+.PARAMETER EnableSSLv12
+
+Optional Parameter
+
+This option enables TLSv1.2 (recommended - check apps work post enabling).
+
+.EXAMPLE
+
+This will enable all TLS version
+
+.\Align_Crypto_Policy.ps1 -LogFilePath c:temp -RegistryExportPath c:\temp -EnableSSLv1 -EnableSSLv11 -EnableSSLv12
+
+
+.EXAMPLE 
+
+This will disable all SSL and enable TLS
+
+.\Align_Crypto_Policy.ps1 -LogFilePath c:temp -RegistryExportPath c:\temp -DisableSSLv3Client -DisableSSLv3Server -EnableSSLv1 -EnableSSLv11 -EnableSSLv12
+
+.EXAMPLE
+
+This will set the ciphers accordingly on the server
+
+.\Align_Crypto_Policy.ps1 -LogFilePath c:temp -RegistryExportPath c:\temp -SetDefaultCiphers
+
+.NOTES
+
+ 1. You need to have Administrative rights on the server to run this script. 
+ 2. This script cannot be run remotely. You will need to copy the folder structure down locally to the server.
+ 3. Please remove the local copy of this script once completed.
+
+
+.LINK
+
+https://github.com/IrtazaChohan/Align-Crypto-Policy
+
+#>
+
+
 
 [CmdletBinding()]
 param (
